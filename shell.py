76759a4ca9c8562c8f6531768a0c8befd6fe7a0b
Jakylan <jstandifer@basecampcodingacademy.org>
2018-07-11 16:25:34 -0500

    Gladiator is now working

---
+++ shell.py
@@ -0,0 +1,54 @@
+import core
+
+
+def gladiator():
+    glad = core.new_gladiator
+    gladiator_1 = glad(100, 0, 10, 25)
+    gladiator_2 = glad(100, 0, 10, 25)
+    while True:
+        print('Gladiator 1', gladiator_1['health'], 'HP', '|||', 'Gladiator 1',
+              gladiator_1['rage'], 'rage')
+        print('Gladiator 2', gladiator_2['health'], 'HP', '|||', 'Gladiator 2',
+              gladiator_2['rage'], 'rage')
+        while True:
+            print('\nGladiator 1 your turn!\n')
+            response = input(
+                '\nGladiator 1 what would you like to attack, heal, pass, or quit?\n'
+            ).strip().lower()
+            if response == 'a' or response == 'attack':
+                core.attack(gladiator_1, gladiator_2)
+                break
+            elif response == 'h' or response == 'heal':
+                core.heal(gladiator_1)
+                break
+            elif response == 'p' or response == 'pass':
+                break
+            elif response == 'q' or response == 'quit':
+                return
+        if core.is_dead(gladiator_2):
+            break
+        while True:
+            print('\nGladiator 2 your turn!\n')
+            response = input(
+                '\nGladiator 2 what would you like to attack, heal, pass, or quit?\n'
+            ).strip().lower()
+            if response == 'a' or response == 'attack':
+                core.attack(gladiator_2, gladiator_1)
+                break
+            elif response == 'h' or response == 'heal':
+                core.heal(gladiator_2)
+                break
+            elif response == 'p' or response == 'pass':
+                break
+            elif response == 'q' or response == 'quit':
+                return
+        if core.is_dead(gladiator_1):
+            break
+
+
+def main():
+    gladiator()
+
+
+if __name__ == '__main__':
+    main()
