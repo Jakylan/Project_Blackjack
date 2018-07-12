ad881c0de11912bf25c8bfb3156b5e44436ffa00
Jakylan <jstandifer@basecampcodingacademy.org>
2018-07-12 14:48:21 -0500

    Gladiator is working

--- shell.py
+++ shell.py
@@ -6,15 +6,14 @@ def gladiator():
     gladiator_1 = glad(100, 0, 10, 25)
     gladiator_2 = glad(100, 0, 10, 25)
     while True:
-        print('Gladiator 1', gladiator_1['health'], 'HP', '|||', 'Gladiator 1',
-              gladiator_1['rage'], 'rage')
-        print('Gladiator 2', gladiator_2['health'], 'HP', '|||', 'Gladiator 2',
-              gladiator_2['rage'], 'rage')
         while True:
             print('\nGladiator 1 your turn!\n')
+            print('Gladiator 1', gladiator_1['health'], 'HP', '|||',
+                  'Gladiator 1', gladiator_1['rage'], 'rage')
             response = input(
                 '\nGladiator 1 what would you like to attack, heal, pass, or quit?\n'
             ).strip().lower()
+
             if response == 'a' or response == 'attack':
                 core.attack(gladiator_1, gladiator_2)
                 break
@@ -26,12 +25,16 @@ def gladiator():
             elif response == 'q' or response == 'quit':
                 return
         if core.is_dead(gladiator_2):
+            print('Gladiator 1 Wins!')
             break
         while True:
             print('\nGladiator 2 your turn!\n')
+            print('Gladiator 2', gladiator_2['health'], 'HP', '|||',
+                  'Gladiator 2', gladiator_2['rage'], 'rage')
             response = input(
                 '\nGladiator 2 what would you like to attack, heal, pass, or quit?\n'
             ).strip().lower()
+            print('--------------------------------------------------')
             if response == 'a' or response == 'attack':
                 core.attack(gladiator_2, gladiator_1)
                 break
@@ -43,6 +46,7 @@ def gladiator():
             elif response == 'q' or response == 'quit':
                 return
         if core.is_dead(gladiator_1):
+            print('Gladiator 2 Wins!')
             break
 
 
